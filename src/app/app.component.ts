86d0658da09cdec1354c8e529673142f28afde8a
howlett-james <yokeshwararao@hotmail.com>
2021-11-08 16:29:31 +0530

    Initial Commi9t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+let fname:string, date:any,phonenumber:number,gender:string,details:string;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  
   title = 'Angular-Practise';
+  genderHasError = true;
+  submitted = false;
+
+  genders = ['Male','Female','Transgender'];
+
+  
+  userModel = new User(fname,date,phonenumber,gender,details);
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateGender(value:any){
+    if(value === 'default'){
+      this.genderHasError = true;
+    }else{
+      this.genderHasError = false;
+    }
+  }
+
+  submit(){
+
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log("Success! ",data),
+      error=> console.error("Error!",error)
+    )
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public name:string,
+        public dob:string,
+        public phone:number,
+        public gender:string,
+        public details:string
+    ){}
+}
